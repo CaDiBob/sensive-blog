53f1b6161926e5ed4206a21e98654761e641ff20
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-01 15:10:30 +0900

    Использовал декоратор @register для регистрации моделей в админ панели

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('likes', 'tags',)
 
 
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ('post',)
 
 
-admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment, CommentAdmin)
